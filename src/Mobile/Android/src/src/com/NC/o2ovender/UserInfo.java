6c9ffd79c8e00633f531bbdfb8638e95826be30e
o2ovender <o2ovender@126.com>
2014-12-30 21:30:04 +0800

    1.添加volley库，测试使用该库进行服务器通信；2.添加userinfo对应的activity；3.添加protobuf，测试使用该工具生成对应的服务器通信命令

---
+++ src/Mobile/Android/src/src/com/NC/o2ovender/UserInfo.java
@@ -0,0 +1,46 @@
+package com.NC.o2ovender;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class UserInfo extends Activity {
+	public final static String TAG = "UserInfo";
+	public static final String U2M_BUNDLE_KEY = "userinfo2main_bundle_key";
+	
+	private Button mActivity2MainBtn;
+	private TextView mTextView;
+	
+	private void InitView() {
+		mTextView = (TextView)findViewById(R.id.UserInfoTextView);
+		Bundle bundle = this.getIntent().getExtras();
+		if (null != bundle) {
+			mTextView.setText(bundle.getString(O2OVenderActivity.M2U_BUNDLE_KEY));
+		}
+		
+		mActivity2MainBtn = (Button)findViewById(R.id.activityo2oBtn);
+		mActivity2MainBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(UserInfo.this, O2OVenderActivity.class);
+				Bundle bundle = new Bundle();
+				bundle.putString(U2M_BUNDLE_KEY, "userinfo to main");
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.userinfo);		
+		InitView();
+	}
+}
